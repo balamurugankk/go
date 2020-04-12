799f36e1e6f75fdc71c4dd2de2e4d14c83d0c1e8
balamurugankk <balamurugankk@gmail.com>
2020-04-12 07:57:55 +0530

    update

--- sample1.go
+++ sample1.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
-import "github.com/balamurugankk/go/models"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/balamurugankk/go/models"
+)
 
 func main() {
 
 	fmt.Println("This my github test code")
 	fmt.Println(models.Codeset2())
 	codeset1()
+	http.HandleFunc("/", codeset3())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func codeset3(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func codeset1() {
